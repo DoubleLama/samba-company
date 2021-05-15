49ae1cd88bf833a052ccd121485d9b6481fae333
DoubleLama <lama31@hotmail.fr>
2021-05-15 11:03:37 +0200

    update api.js for herokuapp.com and samba-company.fr, strasNav on 74rem

--- client/src/api.js
+++ client/src/api.js
@@ -4,11 +4,21 @@ let env = ''
 
 if (process.env.NODE_ENV === 'development') {
   env = 'http://localhost:3000/'
-} else if (process.env.NODE_ENV === 'production') {
-  env =
-    'http://www.samba-company.fr/' ||
-    'http://www.samba-company.com/' ||
-    'https://samba-company.herokuapp.com/'
+} else if (
+  process.env.NODE_ENV === 'production' &&
+  document.location.href.includes('samba-company.com')
+) {
+  env = 'https://www.samba-company.com/'
+} else if (
+  process.env.NODE_ENV === 'production' &&
+  document.location.href.includes('samba-company.fr')
+) {
+  env = 'https://www.samba-company.fr/'
+} else if (
+  process.env.NODE_ENV === 'production' &&
+  document.location.href.includes('herokuapp.com')
+) {
+  env = 'https://samba-company.herokuapp.com/'
 }
 
 export default axios.create({
